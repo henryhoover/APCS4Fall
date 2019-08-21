5700c87ee6ee384de96c4348f2dff08bc1e51185
APCS2nd <APCS2nd@MHS-LT9-18>
2019-08-21 12:16:59 -0700

    complete hello world project

---
+++ HELLO/src/Hello$Money$.java
@@ -0,0 +1,10 @@
+//Henry Wang 
+//8/16/19
+//first class in java for APCS (it prints stuff on screen)
+public class Hello$Money$ {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
